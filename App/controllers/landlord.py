821c8fbc6884ab4eb69c611649ffce5aa57950b9
stu-dent101 <katoya.ottley@my.uwi.edu>
2025-04-15 19:44:22 -0400

    added delete_listing to the landlord controller

--- App/controllers/landlord.py
+++ App/controllers/landlord.py
@@ -9,6 +9,7 @@ def create_landlord(name, email, password, phone_contact):
     new_landlord = Landlord(name=name, email=email, password=password, phone_contact=phone_contact)
     db.session.add(new_landlord)
     db.session.commit()
+    print(f"Landlord {new_landlord.name} has been created")
     return new_landlord
 
 
@@ -21,7 +22,9 @@ def create_listing(landlord_id, apartment_name, apartment_location, number_of_un
         new_apartment = Apartment(apartment_name=apartment_name, apartment_location=apartment_location, number_of_units_avaliable=number_of_units_avaliable, number_of_units_not_avaliable=number_of_units_not_avaliable, apartment_details=apartment_details)
         db.session.add(new_apartment)
         db.session.commit()
+        print(f"Apartment Listing for {new_apartment.apartment_name} has been created")
         return new_apartment
+    
     
 #update_listing
 def update_listing(landlord_id, apartment_id, apartment_name=None, apartment_location=None, number_of_units_avaliable=None, number_of_units_not_avaliable=None, apartment_details=None):
@@ -47,6 +50,19 @@ def update_listing(landlord_id, apartment_id, apartment_name=None, apartment_loc
             valid_apartment.apartment_details = apartment_details
 
         db.session.commit()
+        print(f"Apartment Listing for {valid_apartment.apartment_name} has been updated")
+
 
 #delete_listing
+def delete_listing(landlord_id, apartment_id):
+    valid_landlord = Landlord.query.get(landlord_id)
+    valid_apartment = Apartment.query.get(apartment_id)
+
+    if valid_landlord is not valid_apartment.landlord_id:
+        return None
+    else:
+        db.session.delete(apartment_id)
+        db.session.commit()
+        print(f"Apartment Listing for {valid_apartment.apartment_name} has been deleted")
+
 #get_landlord_apartments
