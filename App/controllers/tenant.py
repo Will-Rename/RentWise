e5aa20761b28c2cd1b3f230fbba96f18e752315d
stu-dent101 <katoya.ottley@my.uwi.edu>
2025-04-16 17:40:59 -0400

    Updated the logic in the tenant controller

--- App/controllers/tenant.py
+++ App/controllers/tenant.py
@@ -1,9 +1,10 @@
-from App.models import Tenant, Review
+from App.models import User, Tenant, Review
 from App.database import db
 
 #create_tenant
 def create_tenant(name, email, password, apartment_id):
-    if Tenant.query.filter_by(email=email).first(): 
+    if User.query.filter_by(email=email).first():
+        print("Email is already taken") 
         return None
     else:
         new_tenant = Tenant(name=name, email=email, password=password, apartment_id=apartment_id)
@@ -14,22 +15,28 @@ def create_tenant(name, email, password, apartment_id):
 
 #create_review
 def create_review(tenant_id, apartment_id, review):
-    if Tenant.query.get(tenant_id): 
+    valid_tenant= Tenant.query.get(tenant_id)
+
+    if not valid_tenant:
+        print("This is not a valid tenant")
         return None
-    else:
-        new_review= Review(tenant_id, apartment_id, review)
-        db.session.add(new_review)
-        db.session.commit()
-        print("Tenant created a review")
-        return new_review
+
+    new_review= Review(tenant_id=tenant_id, apartment_id=apartment_id, review=review)
+    db.session.add(new_review)
+    db.session.commit()
+    print("Tenant created a review")
+    return new_review
 
 #get_tenant_reviews
 def get_tenant_reviews(tenant_id):
-    if Tenant.query.get(tenant_id): 
+    valid_tenant= Tenant.query.get(tenant_id)
+
+    if not valid_tenant:
+        print("This is not a valid tenant")
         return None
-    else:
-        tenant_reviews = Review.query.filter_by(tenant_id=tenant_id).all()
-        return tenant_reviews
+    
+    tenant_reviews = Review.query.filter_by(tenant_id=tenant_id).all()
+    return tenant_reviews
 
 #delete_tenant_review
 def delete_tenant_review(tenant_id, review_id):
@@ -44,7 +51,10 @@ def delete_tenant_review(tenant_id, review_id):
         print ("This is not a valid review")
         return None
     
-    if valid_tenant is valid_review.tenant_id:
-        db.session.delete(review_id)
-        db.session.commit()
-        print(f"Tenant {valid_tenant.name} delected review {valid_review}")
+    if valid_tenant.id != valid_review.tenant_id:
+        print("This tenant is not authorized to delete this review")
+        return None
+    
+    db.session.delete(valid_review)
+    db.session.commit()
+    print(f"Tenant {valid_tenant.name} delected review {valid_review}")
