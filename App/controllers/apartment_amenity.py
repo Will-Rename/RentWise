8c4acd88b43b41c95e90767b73f61863c2500fe4
stu-dent101 <katoya.ottley@my.uwi.edu>
2025-04-16 19:53:37 -0400

    Updated apartment amenity controller logic

--- App/controllers/apartment_amenity.py
+++ App/controllers/apartment_amenity.py
@@ -2,11 +2,15 @@ from App.models import ApartmentAmenities, Apartment, Amenity
 from App.database import db
 
 #add_amenity_to_apartment
-def add_amenity_to_apartment(apartment_id, quantity, amenity_id):
+def add_amenity_to_apartment(apartment_id, quantity, amenity_id, landlord_id):
     valid_apartment = Apartment.query.get(apartment_id)
 
     if not valid_apartment:
         print ("This is not a valid apartment")
+        return None
+    
+    if valid_apartment.landlord_id != landlord_id:
+        print("This landlord is not authorized to add amenities to this apartment")
         return None
     
     existing_amenity = Amenity.query.get(amenity_id) 
@@ -21,25 +25,23 @@ def add_amenity_to_apartment(apartment_id, quantity, amenity_id):
     return new_apartment_amenity
 
 #remove_amenity_from_apartment
-def remove_amenity_from_apartment(apartment_id, amenity_id):
+def remove_amenity_from_apartment(apartment_id, amenity_id, landlord_id):
     valid_apartment_amenity = ApartmentAmenities.query.filter_by(apartment_id=apartment_id, amenity_id=amenity_id).first()
-
-    if not valid_apartment_amenity:
-        print ("This is Apartment does not have this amenity")
-        return None
-    else:
-        db.session.delete(valid_apartment_amenity)
-        db.session.commit()
-        print(f"Amenity {amenity_id.amenity_name} has been removed from apartment {apartment_id.apartment_name}")
-        
-
-#get_apartment_amenities
-def get_apartment_amenities (apartment_id):
     valid_apartment = Apartment.query.get(apartment_id)
 
     if not valid_apartment:
-        print("This is nt a valid apartment")
+        print ("This is not a valid apartment")
+        return None
+
+    if valid_apartment.landlord_id != landlord_id:
+        print("This landlord is not authorized to remove amenities to this apartment")
         return None
     
-    apartment_amenities = ApartmentAmenities.query.all(apartment_id)
-    return apartment_amenities
+    if not valid_apartment_amenity:
+        print ("This is Apartment does not have this amenity")
+        return None
+    
+    db.session.delete(valid_apartment_amenity)
+    db.session.commit()
+    print(f"Amenity {amenity_id.amenity_name} has been removed from apartment {apartment_id.apartment_name}")
+    
