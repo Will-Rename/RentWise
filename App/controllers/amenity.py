ab2024b8945859a1d1288c0eeaf7b202131b462c
stu-dent101 <katoya.ottley@my.uwi.edu>
2025-04-16 19:54:07 -0400

    Update amenity controller logic

--- App/controllers/amenity.py
+++ App/controllers/amenity.py
@@ -2,49 +2,30 @@ from App.models import Amenity, ApartmentAmenities, Landlord, Apartment
 from App.database import db
 
 #create_amenity
-def create_amenity(amenity_name, apartment_id, amenity_id):
-    existing_amenity = ApartmentAmenities.query.filter_by(apartment_id=apartment_id, amenity_id=amenity_id).first() 
+def create_amenity(amenity_name):
+    existing_amenity = Amenity.query.filter_by(amenity_name=amenity_name).first() 
+    
     if existing_amenity:
         print("This amenity is already present")    
         return None
-    else:
-        new_amenity = Amenity(amenity_name=amenity_name)
-        db.session.add(new_amenity)
-        db.session.commit()
-        print(f"New amenity {new_amenity.namenity_nameame} has been created")
-        return new_amenity
+    
+    new_amenity = Amenity(amenity_name=amenity_name)
+    db.session.add(new_amenity)
+    db.session.commit()
+    print(f"New amenity {new_amenity.namenity_nameame} has been created")
+    return new_amenity
 
 #delete_amenity
-def delete_amenity(landlord_id, amenity_id, apartment_id):
-    valid_landlord = Landlord.query.get(landlord_id)
+def delete_amenity(amenity_id):
     valid_amenity = Amenity.query.get(amenity_id)
-    valid_apartment= Apartment.query.get(apartment_id)
-
-    if not valid_apartment:
-        print("This is not a valid apartment")
-        return None
-    
-    if not valid_landlord:
-        print ("This is not a valid landlord")
-        return None
-
-    landlord_apartment = Apartment.query.filter_by(apartment_id=apartment_id, landlord_id=landlord_id).first()
-    if not landlord_apartment:
-        print("This landlord does not own this apartment")
     
     if valid_amenity is None:
         print ("This is not a valid amenity")
         return None
     
-    apartment_amenity = ApartmentAmenities.query.filter_by(apartment_id=apartment_id, amenity_id=amenity_id).first()
-    if not apartment_amenity:
-        print("The apartment does not own this amenity")
-        return None
-    
-
-    db.session.delete(amenity_id)
+    db.session.delete(valid_amenity)
     db.session.commit()
-    print(f"The amenity {valid_amenity.amenity_name} was delected from apartment {valid_apartment.apartment_name}")
+    print(f"The amenity {valid_amenity.amenity_name} was deleted")
 
 
 #get_all_amenities
@@ -55,5 +36,10 @@ def get_all_amenities():
 #get_amenity
 def get_amenity(amenity_id):
     amenity = Amenity.query.get(amenity_id)
-    print("Amenity was found")
+
+    if not amenity:
+        print("This is not valid amenity")
+        return None
+    
+    print(f"Amenity {amenity} was found")
     return amenity
