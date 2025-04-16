6c74f20767459f6894372d7790bd8a5d5908c553
stu-dent101 <katoya.ottley@my.uwi.edu>
2025-04-16 18:43:47 -0400

    Updated the apartment controller logic
    added py to a controller file that was missing it

--- App/controllers/apartment.py
+++ App/controllers/apartment.py
@@ -6,24 +6,60 @@ def get_apartment(apartment_id):
     valid_apartment = Apartment.query.get(apartment_id)
 
     if not valid_apartment:
+        print("This is not a vaild apartment")
         return None
-    else:
-        print(f"Apartment {valid_apartment.apartment_name} Found")
-        return valid_apartment
+    
+    print(f"Apartment {valid_apartment.apartment_name} Found")
+
+    apartment= {
+        "apartment_id": valid_apartment.apartment_id,
+        "apartment_name": valid_apartment.apartment_name,
+        "apartment_location": valid_apartment.apartment_location,
+        "number_of_units_avaliable": valid_apartment.number_of_units_avaliable,
+        "numer_of_units_not_avaliable": valid_apartment.number_of_units_not_avaliable,
+        "apartment_details": valid_apartment.apartment_details,
+        }
+        
+    return apartment
 
 #list_all_apartments
 def list_all_apartments():
     all_apartments = Apartment.query.all()
-    return all_apartments
+
+    list_of_apartments= [
+        {
+        "apartment_id": apartment.apartment_id,
+        "apartment_name": apartment.apartment_name,
+        "apartment_location": apartment.apartment_location,
+        "number_of_units_avaliable": apartment.number_of_units_avaliable,
+        "numer_of_units_not_avaliable": apartment.number_of_units_not_avaliable,
+        "apartment_details": apartment.apartment_details,
+        }
+        for apartment in all_apartments
+    ]
+    return list_of_apartments
 
 #get_apartment_reviews
 def get_apartment_reviews(apartment_id):
-    if Apartment.query.get(apartment_id):
+    valid_apartment = Apartment.query.get(apartment_id)
+
+    if not valid_apartment:
         print("This is not a valid apartment") 
         return None
-    else:
-        apartment_reviews = Review.query.filter_by(apartment_id=apartment_id).all()
-        return apartment_reviews
+    
+    apartment_reviews = Review.query.filter_by(apartment_id=apartment_id).all()
+        
+    list_of_apartment_reviews= [
+        {
+        "review_id": review.review_id,
+        "tenant_id": review.tenant_id,
+        "apartment_id": review.apartment_id,
+        "review": review.review,
+        }
+        for review in apartment_reviews
+    ]
+    return list_of_apartment_reviews
+
 
 #list_apartment_amenities
 def list_apartment_amenities(apartment_id):
@@ -32,6 +68,15 @@ def list_apartment_amenities(apartment_id):
     if not valid_apartment:
         print("This is not a valid Apartment")
         return None
-    else:
-        apartment_amenities = ApartmentAmenities.query.all(apartment_id)
-        return apartment_amenities
+    
+    apartment_amenities = ApartmentAmenities.query.filter_by(apartment_id=apartment_id).all()
+    
+    list_of_apartment_amenities= [
+        {
+            "amenity_id": amenity.amenity_id,
+            "apartment_id": amenity.apartment_id,
+            "amenity_name": amenity.amenity_name,
+         }
+        for amenity in apartment_amenities
+    ]
+    return list_of_apartment_amenities
